9b4bbabaad0ec5f737432e2d3d53014a0ebaa120
hayden365 <ysmh100@gmail.com>
2025-03-27 15:09:15 +0900

    feat: update api routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
-    slug: req.body.slug ? slug : slugify(req.body.title),
+    slug: req.body.slug ? req.body.slug : slugify(req.body.title),
   });
 
   try {
@@ -37,6 +37,29 @@ router.get("/:slug", async (req, res) => {
     res.json(post);
   } catch (err) {
     res.json({ message: err });
+  }
+});
+
+// Update a post
+router.put("/:slug", async (req, res) => {
+  try {
+    const updatedPost = await Post.findOneAndUpdate(
+      { slug: req.params.slug }, // 조건
+      {
+        title: req.body.title,
+        content: req.body.content,
+        slug: req.body.slug ? req.body.slug : slugify(req.body.title),
+      },
+      { new: true } // 수정 후 문서를 반환
+    );
+
+    if (!updatedPost) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.json(updatedPost);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
