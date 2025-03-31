794d9f267976f45ce594294b037b7181d6493cee
hayden365 <ysmh100@gmail.com>
2025-04-01 00:13:00 +0900

    chore: 디자인 수정

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,9 +5,5 @@ import { getPosts } from "./lib/api/posts";
 export default async function Home() {
   const initialPosts = await getPosts();
 
-  return (
-    <div className="flex justify-center pt-[50px]">
-      <PostList initialPosts={initialPosts} />
-    </div>
-  );
+  return <PostList initialPosts={initialPosts} />;
 }
