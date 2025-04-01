6577cf20c3824bb193c87579d625c6dc892095f4
hayden365 <ysmh100@gmail.com>
2025-04-01 21:22:50 +0900

    feat: ssg revalidate 60

--- client/src/app/lib/api/fetch.ts
+++ client/src/app/lib/api/fetch.ts
@@ -2,7 +2,9 @@ const API_URL = process.env.NEXT_PUBLIC_URL;
 
 export async function fetchPostList() {
   const res = await fetch(`${API_URL}/posts`, {
-    cache: "no-store",
+    next: {
+      revalidate: 60,
+    },
   });
   if (!res.ok) throw new Error("Failed to fetch posts");
   return res.json();
@@ -10,7 +12,9 @@ export async function fetchPostList() {
 
 export async function fetchPost(slug: string) {
   const res = await fetch(`${API_URL}/posts/${slug}`, {
-    cache: "no-store",
+    next: {
+      revalidate: 60,
+    },
   });
   if (!res.ok) throw new Error("Failed to fetch post");
   return res.json();
